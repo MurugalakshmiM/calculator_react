58fd6719746c7d4f4e048bcf9d307661878b2fdf
MurugalakshmiM <priyareddy3298@outlook.com>
2019-05-13 11:02:19 +0530

    calculator-react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class App extends React.Component{
+  arr = [1,2,3,'+',4,5,6,'-',7,8,9,'*',0,'/','=','c'];
+  buttons = this.arr.map((x) => <button onClick={() => this.clickfunc(x)} key = {x.toString()} >{x}</button> );
+  constructor(probs) {
+    super(probs);
+    this.state = {value : ""};
+  }
+  clickfunc(x){
+    if(x === 'c'){
+          this.setState({value : ""});
+    }
+    else if(x !== '=') {
+            this.setState({value : this.state.value + x});
+    }
+    else{
+          if(this.state.value){
+            this.setState({value : eval(this.state.value)});
+          }
+    }
 
-function App() {
+  }
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="board1">
+        <input type="text" value={this.state.value} disabled ></input>
+      </div>
+      <div className="board">
+        {this.buttons}
+      </div>
     </div>
   );
 }
+ }
 
 export default App;
