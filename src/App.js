43599b9ddff0c19f127f359b285f74f9680d5629
MurugalakshmiM <priyareddy3298@outlook.com>
2019-05-13 21:53:25 +0530

    redux-react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import Component1 from './component/component1'
+import { conditionalExpression } from '@babel/types';
+
 
 class App extends React.Component {
   arr = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, '/', '=', 'c'];
@@ -15,19 +17,24 @@ class App extends React.Component {
       this.setState({ value: "" });
     }
     else if (x !== '=') {
-      this.setState({ value: this.state.value + x });
+      this.setState({ value: this.state.value + x },()=>{
+        this.props.display(this.state.value)
+      });
     }
     else {
       if (this.state.value) {
         try {
-          this.setState({ value: eval(this.state.value) });
+          this.setState({ value: eval(this.state.value)},()=>{
+            this.props.display(this.state.value)
+          });
         } catch (e) {
-          this.setState({ value: "" });
+          this.setState({ value: "" },()=>{
+            this.props.display(this.state.value)
+          });
           window.alert("syntax error");
         }
       }
     }
-    setTimeout(()=> this.props.display(this.state.value),0)
    
   }
   render() {
